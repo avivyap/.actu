a97fc819892438bcb1018612c12f71a925b2233d
aviv <141031195+avivyap@users.noreply.github.com>
2025-10-25 15:32:02 +0000

    Update actu.sh

--- actu.sh
+++ actu.sh
@@ -12,45 +12,45 @@ grayColour="\e[0;37m\033[1m"
 
 trap ctrl_c INT
 function ctrl_c(){
-	echo -e "\n${redColour}[+] Saliendo........${endColour}\n"
-	tput cnorm; exit 1
+        echo -e "\n${redColour}[+] Saliendo........${endColour}\n"
+        tput cnorm; exit 1
 }
 
 
 function no_actu(){
 
-	echo -e "\n${purpleColour}[+]${endColour}${yellowColour} Hay varios paquetes que no se pueden actualizar, asi que tienes que hacerlo a mano${endColour}\n"
-	apt list --upgradable 2>/dev/null
-	echo -e "\n${purpleColour}[+]${endColour}${yellowColour} [+] Para actualizarlos es --->${endColour}${blueColour}   sudo apt upgrade x ${endColour}${yellowColour}<- x es el nombre del paquete${endColour}\n"
-	tput cnorm; exit 0
+        echo -e "\n${purpleColour}[+]${endColour}${yellowColour} Hay varios paquetes que no se pueden actualizar, asi que tienes que hacerlo a mano${endColour}\n"
+        apt list --upgradable 2>/dev/null
+        echo -e "\n${purpleColour}[+]${endColour}${yellowColour} [+] Para actualizarlos es --->${endColour}${blueColour}   sudo apt upgrade x ${endColour}${yellowColour}<- x es el nombre del paquete${endColour}\n"
+        tput cnorm; exit 0
 }
 
 function actualizar(){
 
-	echo -e "\n${purpleColour}[+]${endColour} Se han encontrado paquetes sin actualizar${endColour}\n"
-	sudo apt upgrade -y > /dev/null 2>&1
-	comprobacion
+        echo -e "\n${purpleColour}[+]${endColour}${yellowColour} Se han encontrado paquetes sin actualizar${endColour}\n"
+        sudo apt upgrade -y > /dev/null 2>&1
+        comprobacion
 
 }
 function todo_actu(){
 
-	echo -e "\n${purpleColour}[+]${endColour}${greenColour} Todo esta actualizado${endColor}\n"
-	tput cnorm; exit 0
+        echo -e "\n${purpleColour}[+]${endColour}${greenColour} Todo esta actualizado${endColor}\n"
+        tput cnorm; exit 0
 }
 
 function comprobacion(){
-	((trys++))
-	if [ $trys == 3 ];then
+        ((trys++))
+        if [ $trys == 3 ];then
 
-		no_actu
+                no_actu
 
-	else
-		if [ "$(sudo apt update 2>/dev/null | tail -n 1)" == "Todos los paquetes están actualizados." ];then
-			todo_actu
-		else
-			actualizar
-		fi
-	fi
+        else
+                if [ "$(sudo apt update 2>/dev/null | tail -n 1)" == "Todos los paquetes están actualizados." ];then
+                        todo_actu
+                else
+                        actualizar
+                fi
+        fi
 }
 
 
@@ -60,9 +60,9 @@ declare -i trys=0
 tput civis;clear
 if [ $(id -u) == "0" ];then
 
-	comprobacion
+        comprobacion
 
 else
-	echo -e "\n${purpleColour}[+]${endColour}${redColour} No estas ejecutando el script como root${endColour}\n"
-	tput cnorm; exit 1
+        echo -e "\n${purpleColour}[+]${endColour}${redColour} No estas ejecutando el script como root${endColour}\n"
+        tput cnorm; exit 1
 fi
